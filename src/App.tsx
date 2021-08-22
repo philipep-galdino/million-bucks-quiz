4e10eaf0dc4baedbf5fdd7cc71370008637d199c
Philipe <philipep.galdino@gmail.com>
2021-08-22 10:47:05 -0300

    Initiated App Structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from 'react'
+
+import MainContainer from './components/Main'
+
+const App: FunctionComponent = () => {
+  return (
+    <div>
+      <MainContainer />
+    </div>
+  )
+}
+
+export default App

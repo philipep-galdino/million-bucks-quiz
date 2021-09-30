d7ba2f4ea377f82126e6ed67a1f595464c26ad57
Philipe <philipep.galdino@gmail.com>
2021-09-30 17:19:01 -0300

    changed app name

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createGlobalStyle, ThemeProvider } from 
+import { createGlobalStyle, ThemeProvider } from
 'styled-components'
 import Head from 'next/head'
 import db from '../db.json'
@@ -27,7 +27,7 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
-const { theme, bg, title, description } = db; 
+const { theme, bg, title, description } = db;
 
 export default function App({ Component, pageProps }) {
   return (
@@ -36,26 +36,15 @@ export default function App({ Component, pageProps }) {
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous" />
-                        
-            <title>Kimetsu Quiz</title>
+
+            <title>Show do Milhão Direitíssimo</title>
 
             <meta name="title" content={title} />
             <meta name="description" content={description} />
 
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://kimetsuquiz-base.lilitbandeira.vercel.app/" />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:image" content={bg} />
-
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content="https://kimetsuquiz-base.lilitbandeira.vercel.app/" />
-            <meta property="twitter:title" content={title} />
-            <meta property="twitter:description" content={description} />
-            <meta property="twitter:image" content={bg} />
         </Head>
         <ThemeProvider theme={theme}>
-            <GlobalStyle /> 
+            <GlobalStyle />
             <Component {...pageProps} />
         </ThemeProvider>
     </>
